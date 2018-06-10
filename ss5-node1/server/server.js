d959e675ee759842a5fb28f368e64b06db9ee321
tranlam076 <tranlam076@gmail.com>
2018-06-10 16:31:29 +0700

    thierd commit

--- ss5-node1/server/server.js
+++ ss5-node1/server/server.js
@@ -30,6 +30,13 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app
+    .use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+app
     .use(bodyParser.json()) // Execute every single request
     .use(bodyParser.urlencoded({extended: true}));
 
